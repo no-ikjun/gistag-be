d4cd8b49829b4a092cc06f6c02faf5db93038d2d
hyunseo03 <myhyun0103@gmail.com>
2026-05-04 16:34:40 +0900

    ✨ feat: add PostgreSQL support by including pg and related dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './global/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '1234',
+      database: 'gistag',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
